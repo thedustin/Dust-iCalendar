01c0b85c17a9746d0f1e9b4907f6c6ad7803f05f
Dustin <dustin.breuer@gmail.com>
2013-10-07 18:29:26 +0200

    Add CalendarDocument
    i want a nice solution to use the right linebreak

---
+++ src/Dust/Calendar/iCalendar/Document/Document.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * PHP version 5
+ *
+ * @category   Dust-iCalendar
+ * @package    Dust\Calendar\iCalendar\Document
+ * @subpackage Document
+ * @author     Dustin Breuer <dustin.breuer@googlemail.com>
+ * @license    MIT License
+ * @link       http://thedust.in
+ */
+
+namespace Dust\Calendar\iCalendar\Document;
+
+use Dust\Calendar\iCalendar\Collection\Collection;
+
+/**
+ * Dust-iCalendar Document
+ *
+ * @category   Dust-iCalendar
+ * @package    Dust\Calendar\iCalendar\Document
+ * @subpackage Document
+ * @author     Dustin Breuer <dustin.breuer@googlemail.com>
+ * @copyright  2013 Dustin Breuer <dustin.breuer@googlemail.com>
+ * @license    MIT License
+ * @version    Release: @package_version@
+ * @link       http://thedust.in
+ */
+class Document extends Collection {
+
+    const EOL = "\r\n";
+
+}
