47463bda320dac49344ecd842c5122d893ccf21d
Dustin <dustin.breuer@gmail.com>
2013-10-10 17:28:05 +0200

    Forgot to add this with #4971ef9

---
+++ tests/init.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * PHP version 5
+ *
+ * @category   Dust-iCalendar
+ * @package    Dust\Calendar
+ * @subpackage Tests
+ * @author     Dustin Breuer <dustin.breuer@googlemail.com>
+ * @license    MIT License
+ * @link       http://thedust.in
+ */
+
+$aPaths   = explode(PATH_SEPARATOR, get_include_path());
+$aPaths[] = realpath(__DIR__ . '/../');
+set_include_path(implode(PATH_SEPARATOR, $aPaths));
+
+spl_autoload_register(function ($sClass) {
+    $sClass = str_replace('\\', '/', $sClass);
+
+    include 'src/' . $sClass . '.php';
+});
+
+require_once 'PHPUnit/Autoload.php';
